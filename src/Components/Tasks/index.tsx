0b48d87385f5848fdc0159517383a94d4395b41a
CaioVHilario <caio.hilario@ufv.br>
2024-06-04 11:36:32 -0300

    add slider progress

--- src/Components/Tasks/index.tsx
+++ src/Components/Tasks/index.tsx
@@ -1,8 +1,9 @@
-import React from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Image, TouchableOpacity, Alert, Modal } from "react-native";
 import { styles } from "./styles";
 import { ImageCFHF, ImageCTHF } from "../../../assets";
 import Ionicons from '@expo/vector-icons/Ionicons'
+import Slider from "@react-native-community/slider";
 
 type props = {
     text: string;
@@ -13,11 +14,67 @@ type props = {
 }
 
 export default function Tasks({text, done, onRemove, onDone, onChange}: props){
+    const [modalVisible, setModalVisible] = useState(false);
+    const [range, setRange] = useState(0);
+    const [isFocused, setFocused] = useState(false);
+
+
+    function handleModal(){
+        setModalVisible(true)
+    }
+
+    function handleFocus(){
+        setFocused(!isFocused)
+     }
+
+     function handleRangeDone(){
+        setRange(100)
+     }
 
     return(
-        <View style={styles.newTask}>
-            
-            <TouchableOpacity style={styles.checkedButon} onPress={onDone}>
+        <View style={[styles.newTask, {
+            borderColor: isFocused === true
+             ? '#5E60CE'
+             : '#292929'
+        }]} >
+
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                Alert.alert('Modal has been closed.');
+                setModalVisible(!modalVisible);
+            }}>
+
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>Como está o andamento da sua tarefa?</Text>
+                        {/* <View style={styles.slider}>
+                            <Text style={styles.textSlider}> 100% </Text>
+                        </View> */}
+                        <Text style={styles.modalText}>{range}%</Text>
+                        <Slider 
+                            style={styles.slider}
+                            minimumValue={0}
+                            maximumValue={100}
+                            minimumTrackTintColor="#1e6f9f"
+                            maximumTrackTintColor="#808080"
+                            thumbTintColor="#1e6f9f"
+                            value={range}
+                            onValueChange={value => setRange(parseInt(value))}
+                        />
+                        <TouchableOpacity
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => {setModalVisible(!modalVisible); handleFocus()}}>
+                        <Text style={styles.textStyle}>Hide Modal</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+            </Modal>
+                
+            <TouchableOpacity style={styles.checkedButon} onPress={() => {onDone(); handleRangeDone()}}>
                 { done === false ? <Image 
                     style={styles.Unchecked}
                     source={ImageCFHF}
@@ -28,13 +85,26 @@ export default function Tasks({text, done, onRemove, onDone, onChange}: props){
                 />}
             </TouchableOpacity>
 
-            <TouchableOpacity style={styles.task} onPress={onChange}>
-                { done === false ? <Text style={styles.addText}> 
-                    {text} 
-                </Text> : 
-                <Text style={styles.addTextChecked}> 
-                    {text} 
-                </Text>}
+            <TouchableOpacity style={styles.task} onPress={() => {handleModal(); handleFocus()}}>
+                { done === false ? 
+                    <View style={styles.taskRange}>
+                        <Text style={styles.addText}> 
+                            {text} 
+                        </Text>  
+                        <Text style={styles.addTextRange}> 
+                            {range}%
+                        </Text> 
+                    </View> : 
+                    <View style={styles.taskRange}>
+                        <Text style={styles.addTextChecked}> 
+                            {text} 
+                        </Text>
+                        <Text style={styles.addTextRangeChecked}> 
+                            {range}% 
+                        </Text>
+                    </View>
+                }
+                
             </TouchableOpacity>
             
             <TouchableOpacity style={styles.trashButon} onPress={onRemove}>
--- src/Components/Tasks/styles.ts
+++ src/Components/Tasks/styles.ts
@@ -23,6 +23,8 @@ export const styles = StyleSheet.create({
         flexDirection: "row",
         alignItems: 'center',
         borderRadius: 8,
+        // borderColor: "#fff",
+        borderWidth: 1,
         height: 64,
         marginBottom: 20,
         backgroundColor: "#262626",
@@ -37,7 +39,8 @@ export const styles = StyleSheet.create({
     },
     addText: {
         fontSize: 14,
-        color: "#fff"
+        color: "#fff",
+        width: '50%',
     },
     task: {
         width: "70%",
@@ -47,7 +50,69 @@ export const styles = StyleSheet.create({
     },
     addTextChecked: {
         fontSize: 14,
+        width: '50%',
         color: "#808080",
         textDecorationLine: 'line-through'
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "flex-end",
+      },
+    modalView: {
+        backgroundColor: '#000',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+    },
+    buttonClose: {
+        backgroundColor: '#1e6f9f',
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    modalText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 15,
+        color: "#fff",
+        textAlign: 'center',
+    },
+    slider: {
+        height: 45,
+        width: "100%",
+        marginBottom: 15,
+        borderRadius: 50,
+        justifyContent: "center"
+    },
+    textSlider: {
+        fontSize: 16,
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    addTextRange: {
+        fontSize: 14,
+        width: '50%',
+        fontWeight: "bold",
+        color: "#fff",
+        textAlign: 'right'
+    },
+    addTextRangeChecked: {
+        fontSize: 14,
+        width: '50%',
+        fontWeight: "bold",
+        color: "#808080",
+        textDecorationLine: 'line-through',
+        textAlign: 'right'
+    },
+    taskRange: {
+        flexDirection: 'row',
     }
 })
